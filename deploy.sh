ac88cb6da62805f02bea3c4c5dbfc8aa5652ee60
Craig <craigtb@gmail.com>
2016-09-11 01:53:27 +0000

    Deploy script to move files to apache location

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#sudo cp -rf ./* /var/www/html/
+sudo cp -rf ./node_modules/* /var/www/html/node_modules/
+sudo cp *.html /var/www/html/
+sudo cp -rf ./js/* /var/www/html/js/
+#sudo cp ./js/controllers/* /var/www/html/js/controllers/
